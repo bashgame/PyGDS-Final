a2dfb642ca43c8e58574b38d07030e79e1f277c9
bashgame <amen.kester@gmail.com>
2023-02-27 16:59:09 -0500

    Renamed test.sh
    Renamed test.sh to runtests.sh for better clarity on script purpose and to improve bash autocomplete

--- test.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-flake8 source --count --select=E9,F63,F7,F82 --show-source --statistics
-flake8 source --count --max-complexity=10 --max-line-length=127 --statistics
-
-nosetests -v --with-spec --spec-color --with-coverage --cover-package=source
